360e124baa1e4aee594bfdb407851df0f774e835
Morgan Galvin <morgan@ludwigthepig.com>
2020-12-29 11:47:22 -0700

    init commit
    I think everything is setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './components/**/*.js',
+    './components/**/*.ts',
+    './pages/**/*.js',
+    './pages/**/*.ts',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
